b58f89b77dc463a781c08bdf81ab5be219b969cf
Rushil Mahadevu <rushil.mahadevu@gmail.com>
2024-12-14 17:16:16 -0800

    College Matcher Complete with Gemini a headache out of the way and api public key fix

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    OPENAI_API_KEY: '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'
-  };
-  
-  export default config;
-  
